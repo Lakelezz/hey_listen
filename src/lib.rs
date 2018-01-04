e4a2e1ceb28b60e790682fd523d0bfeb1f5c38be
Lakelezz <Lakelezz@users.noreply.github.com>
2018-01-04 17:20:01 +0100

    Add priority based event-dispatcher.

--- src/lib.rs
+++ src/lib.rs
@@ -52,11 +52,12 @@ extern crate parking_lot;
 
 use std::sync::{Weak, Arc};
 use std::hash::Hash;
-use std::collections::HashMap;
+use std::collections::{HashMap, BTreeMap};
 use std::marker::Send;
 use parking_lot::Mutex;
 
 type ListenerMap<T> = HashMap<T, Vec<Weak<Mutex<Listener<T>>>>>;
+type PriorityListenerMap<P, T> = HashMap<T, BTreeMap<P, Vec<Weak<Mutex<Listener<T>>>>>>;
 
 /// Every event-receiver needs to implement this trait
 /// in order to receive dispatched events.
@@ -192,3 +193,145 @@ impl<T> EventDispatcher<T>
         }
     }
 }
+
+
+/// Owns a map of all listened event-variants and
+/// [`Weak`]-references to their listeners but opposed to
+/// [`EventListener`], this structure utilises one [`BTreeMap`] per
+/// event-type to order listeners by a given priority-level.
+///
+/// [`Weak`]: https://doc.rust-lang.org/std/sync/struct.Weak.html
+/// [`BTreeMap`]: https://doc.rust-lang.org/std/collections/struct.BTreeMap.html
+/// [`EventListener`]: struct.EventDispatcher.html
+#[derive(Default)]
+pub struct PriorityEventDispatcher<P, T>
+    where P: Ord,
+        T: PartialEq + Eq + Hash + Clone + Send + 'static {
+    events: PriorityListenerMap<P, T>
+}
+
+unsafe impl<P: Ord, T: PartialEq + Eq + Hash + Clone + Send + 'static> Send for PriorityEventDispatcher<P, T> {}
+
+impl<P, T> PriorityEventDispatcher<P, T>
+    where P: Ord,
+        T: PartialEq + Eq + Hash + Clone + Send + 'static {
+    pub fn new() -> EventDispatcher<T> {
+        EventDispatcher {
+            events: ListenerMap::new()
+        }
+    }
+
+    /// Adds a [`Listener`] to listen for an `event_identifier`.
+    /// If `event_identifier` is a new [`HashMap`]-key, it will be added.
+    ///
+    /// **Note**: If your `Enum` owns fields you need to consider implementing
+    /// the [`Hash`]- and [`PartialEq`]-trait if you want to ignore fields,
+    /// see second example for an implementation-suggestion.
+    ///
+    /// # Examples
+    ///
+    /// Adding a [`Listener`] to the dispatcher:
+    ///
+    /// ```rust
+    /// extern crate hey_listen;
+    /// extern crate parking_lot;
+    /// use std::sync::Arc;
+    ///
+    /// use hey_listen::Listener;
+    /// use hey_listen::EventDispatcher;
+    ///
+    /// #[derive(Clone, Eq, Hash, PartialEq)]
+    /// enum EventEnum {
+    ///     EventVariant,
+    /// }
+    ///
+    /// struct ListenerStruct {}
+    ///
+    /// impl Listener<EventEnum> for ListenerStruct {
+    ///     fn on_event(&mut self, event: &EventEnum) {}
+    /// }
+    ///
+    /// fn main() {
+    ///     let listener = Arc::new(parking_lot::Mutex::new(ListenerStruct {}));
+    ///     let mut dispatcher: EventDispatcher<EventEnum> = EventDispatcher::new();
+    ///
+    ///     dispatcher.add_listener(EventEnum::EventVariant, &listener);
+    /// }
+    /// ```
+    ///
+    /// Declaring your own [`Hash`]- and [`PartialEq`]-trait to bypass
+    /// hashing on fields:
+    ///
+    /// ```rust
+    /// use std::hash::{Hash, Hasher};
+    /// use std::mem::discriminant;
+    ///
+    /// #[derive(Clone)]
+    /// enum Test {
+    ///     TestVariant(i32),
+    /// }
+    ///
+    /// impl Hash for Test {
+    ///     fn hash<H: Hasher>(&self, _state: &mut H) {}
+    /// }
+    ///
+    /// impl PartialEq for Test {
+    ///     fn eq(&self, other: &Test) -> bool {
+    ///         discriminant(self) == discriminant(other)
+    ///     }
+    /// }
+    ///
+    /// impl Eq for Test {}
+    /// ```
+    ///
+    /// [`Listener`]: trait.Listener.html
+    /// [`Hash`]: https://doc.rust-lang.org/std/hash/trait.Hash.html
+    /// [`PartialEq`]: https://doc.rust-lang.org/std/cmp/trait.PartialEq.html
+    /// [`HashMap`]: https://doc.rust-lang.org/std/collections/struct.HashMap.html
+    pub fn add_listener<D: Listener<T> + 'static>(&mut self, event_identifier: T, listener: &Arc<Mutex<D>>, priority: P) {
+        if let Some(prioritised_listener_collection) = self.events.get_mut(&event_identifier) {
+            if let Some(priority_level_collection) = prioritised_listener_collection.get_mut(&priority) {
+                priority_level_collection.push(Arc::downgrade(&(Arc::clone(listener) as Arc<Mutex<Listener<T>>>)));
+
+                return;
+            }
+        }
+
+        let mut b_tree_map = BTreeMap::new();
+        b_tree_map.insert(priority, vec!(Arc::downgrade(&(Arc::clone(listener) as Arc<Mutex<Listener<T>>>))));
+        self.events.insert(event_identifier, b_tree_map);
+    }
+
+    /// All [`Listener`]s listening to a passed `event_identifier`
+    /// will be called via their implemented [`on_event`]-method.
+    ///
+    /// **Notice**: [`Listener`]s will called ordered by their priority-level.
+    ///
+    /// [`Listener`]: trait.Listener.html
+    /// [`on_event`]: trait.Listener.html#tymethod.on_event
+    pub fn dispatch_event(&mut self, event_identifier: &T) {
+        if let Some(prioritised_listener_collection) = self.events.get_mut(event_identifier) {
+
+            for (_, listener_collection) in prioritised_listener_collection.iter_mut() {
+                let mut found_invalid_weak_ref = false;
+
+                for listener in listener_collection.iter() {
+
+                    if let Some(listener_rc) = listener.upgrade() {
+                        let mut listener = listener_rc.lock();
+                        listener.on_event(event_identifier);
+                    } else {
+                        found_invalid_weak_ref = true;
+                    }
+                }
+
+                if found_invalid_weak_ref {
+                    listener_collection.retain(|listener| {
+                        Weak::clone(listener).upgrade().is_some()
+                    });
+                }
+            }
+        }
+    }
+}
+
