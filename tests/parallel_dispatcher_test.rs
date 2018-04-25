d1bf6d8baec231433bc4af4f8511fd296da60c74
Lakelezz <Lakelezz@users.noreply.github.com>
2018-04-25 16:03:23 +0200

    Add tests for parallel dispatcher. Tests for dispatching trait objects and closures. Also tests for stop listening returned by closures and trait objects.

---
+++ tests/parallel_dispatcher_test.rs
@@ -0,0 +1,210 @@
+extern crate hey_listen;
+extern crate parking_lot;
+
+use hey_listen::{ParallelDispatcherRequest, ParallelEventDispatcher, ParallelListener};
+use std::sync::Arc;
+use parking_lot::Mutex;
+
+#[derive(Clone, Eq, Hash, PartialEq)]
+enum Event {
+    VariantA,
+    VariantB,
+}
+
+#[test]
+fn dispatch_parallel_to_dyn_traits() {
+    #[derive(Default)]
+    struct CountingEventListener {
+        dispatch_counter: usize,
+    }
+
+    impl ParallelListener<Event> for CountingEventListener {
+        fn on_event(&mut self, _event: &Event) -> Option<ParallelDispatcherRequest> {
+            self.dispatch_counter += 1;
+
+            None
+        }
+    }
+
+    let mut dispatcher = ParallelEventDispatcher::<Event>::default();
+    let listener_a = Arc::new(Mutex::new(CountingEventListener::default()));
+    let listener_b = Arc::new(Mutex::new(CountingEventListener::default()));
+
+    {
+        dispatcher.add_listener(Event::VariantA, &listener_a);
+        dispatcher.add_listener(Event::VariantB, &listener_b);
+    }
+
+    assert_eq!(listener_a.try_lock().unwrap().dispatch_counter, 0);
+    assert_eq!(listener_b.try_lock().unwrap().dispatch_counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    assert_eq!(listener_a.try_lock().unwrap().dispatch_counter, 1);
+    assert_eq!(listener_b.try_lock().unwrap().dispatch_counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    assert_eq!(listener_a.try_lock().unwrap().dispatch_counter, 2);
+    assert_eq!(listener_b.try_lock().unwrap().dispatch_counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    dispatcher.dispatch_event(&Event::VariantB);
+    assert_eq!(listener_a.try_lock().unwrap().dispatch_counter, 3);
+    assert_eq!(listener_b.try_lock().unwrap().dispatch_counter, 1);
+
+    dispatcher.dispatch_event(&Event::VariantB);
+    assert_eq!(listener_a.try_lock().unwrap().dispatch_counter, 3);
+    assert_eq!(listener_b.try_lock().unwrap().dispatch_counter, 2);
+}
+
+#[test]
+fn dispatch_parallel_to_functions() {
+    let mut dispatcher = ParallelEventDispatcher::<Event>::default();
+
+    #[derive(Default)]
+    struct DispatchCounter {
+        counter: usize,
+    };
+
+    let counter_a = Arc::new(Mutex::new(DispatchCounter::default()));
+    let counter_b = Arc::new(Mutex::new(DispatchCounter::default()));
+
+    let weak_counter_ref = Arc::downgrade(&Arc::clone(&counter_a));
+    let closure_a = Arc::new(move |_event: &Event| {
+        let listener = &weak_counter_ref.upgrade().unwrap();
+        listener.try_lock().unwrap().counter += 1;
+
+        None
+    });
+
+    let weak_counter_ref = Arc::downgrade(&Arc::clone(&counter_b));
+    let closure_b = Arc::new(move |_event: &Event| {
+        let listener = &weak_counter_ref.upgrade().unwrap();
+        listener.try_lock().unwrap().counter += 1;
+
+        None
+    });
+
+    {
+        dispatcher.add_fn(Event::VariantA, closure_a);
+        dispatcher.add_fn(Event::VariantB, closure_b);
+    }
+
+    assert_eq!(counter_a.try_lock().unwrap().counter, 0);
+    assert_eq!(counter_b.try_lock().unwrap().counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    assert_eq!(counter_a.try_lock().unwrap().counter, 1);
+    assert_eq!(counter_b.try_lock().unwrap().counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    assert_eq!(counter_a.try_lock().unwrap().counter, 2);
+    assert_eq!(counter_b.try_lock().unwrap().counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    dispatcher.dispatch_event(&Event::VariantB);
+    assert_eq!(counter_a.try_lock().unwrap().counter, 3);
+    assert_eq!(counter_b.try_lock().unwrap().counter, 1);
+
+    dispatcher.dispatch_event(&Event::VariantB);
+    assert_eq!(counter_a.try_lock().unwrap().counter, 3);
+    assert_eq!(counter_b.try_lock().unwrap().counter, 2);
+}
+
+#[test]
+fn stop_listening_parallel_for_dyn_traits() {
+    #[derive(Default)]
+    struct CountingEventListener {
+        dispatch_counter: usize,
+    }
+
+    impl ParallelListener<Event> for CountingEventListener {
+        fn on_event(&mut self, _event: &Event) -> Option<ParallelDispatcherRequest> {
+            self.dispatch_counter += 1;
+
+            Some(ParallelDispatcherRequest::StopListening)
+        }
+    }
+
+    let mut dispatcher = ParallelEventDispatcher::<Event>::default();
+    let listener_a = Arc::new(Mutex::new(CountingEventListener::default()));
+    let listener_b = Arc::new(Mutex::new(CountingEventListener::default()));
+
+    {
+        dispatcher.add_listener(Event::VariantA, &listener_a);
+        dispatcher.add_listener(Event::VariantB, &listener_b);
+    }
+
+    assert_eq!(listener_a.try_lock().unwrap().dispatch_counter, 0);
+    assert_eq!(listener_b.try_lock().unwrap().dispatch_counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    assert_eq!(listener_a.try_lock().unwrap().dispatch_counter, 1);
+    assert_eq!(listener_b.try_lock().unwrap().dispatch_counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    assert_eq!(listener_a.try_lock().unwrap().dispatch_counter, 1);
+    assert_eq!(listener_b.try_lock().unwrap().dispatch_counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    dispatcher.dispatch_event(&Event::VariantB);
+    assert_eq!(listener_a.try_lock().unwrap().dispatch_counter, 1);
+    assert_eq!(listener_b.try_lock().unwrap().dispatch_counter, 1);
+
+    dispatcher.dispatch_event(&Event::VariantB);
+    assert_eq!(listener_a.try_lock().unwrap().dispatch_counter, 1);
+    assert_eq!(listener_b.try_lock().unwrap().dispatch_counter, 1);
+}
+
+#[test]
+fn stop_listening_parallel_for_fns() {
+    let mut dispatcher = ParallelEventDispatcher::<Event>::default();
+
+    #[derive(Default)]
+    struct DispatchCounter {
+        counter: usize,
+    };
+
+    let counter_a = Arc::new(Mutex::new(DispatchCounter::default()));
+    let counter_b = Arc::new(Mutex::new(DispatchCounter::default()));
+
+    let weak_counter_ref = Arc::downgrade(&Arc::clone(&counter_a));
+    let closure_a = Arc::new(move |_event: &Event| {
+        let listener = &weak_counter_ref.upgrade().unwrap();
+        listener.try_lock().unwrap().counter += 1;
+
+        None
+    });
+
+    let weak_counter_ref = Arc::downgrade(&Arc::clone(&counter_b));
+    let closure_b = Arc::new(move |_event: &Event| {
+        let listener = &weak_counter_ref.upgrade().unwrap();
+        listener.try_lock().unwrap().counter += 1;
+
+        None
+    });
+
+    {
+        dispatcher.add_fn(Event::VariantA, closure_a);
+        dispatcher.add_fn(Event::VariantB, closure_b);
+    }
+
+    assert_eq!(counter_a.try_lock().unwrap().counter, 0);
+    assert_eq!(counter_b.try_lock().unwrap().counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    assert_eq!(counter_a.try_lock().unwrap().counter, 1);
+    assert_eq!(counter_b.try_lock().unwrap().counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    assert_eq!(counter_a.try_lock().unwrap().counter, 2);
+    assert_eq!(counter_b.try_lock().unwrap().counter, 0);
+
+    dispatcher.dispatch_event(&Event::VariantA);
+    dispatcher.dispatch_event(&Event::VariantB);
+    assert_eq!(counter_a.try_lock().unwrap().counter, 3);
+    assert_eq!(counter_b.try_lock().unwrap().counter, 1);
+
+    dispatcher.dispatch_event(&Event::VariantB);
+    assert_eq!(counter_a.try_lock().unwrap().counter, 3);
+    assert_eq!(counter_b.try_lock().unwrap().counter, 2);
+}
