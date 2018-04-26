5a3fece3547416d575ea672b2091f777f00caeac
Lakelezz <Lakelezz@users.noreply.github.com>
2018-04-26 02:44:30 +0200

    Add example for prioritised dispatcher.

---
+++ examples/sync_prioritised_dispatcher/src/main.rs
@@ -0,0 +1,105 @@
+//! This example shows you how to use the synchronous prioritised event-dispatcher: `PriorityEventDispatcher`.
+//! We will dispatch enumes to trait-objects and closures.
+//!
+//! The `PriorityEventDispatcher` itself can only dispatch to one listener at a time,
+//! thus referred to as synchronous.
+//! We will dispatch to lower priority-levels first.
+//!
+//! I will often use the term `listener`, it describes an event-receiver,
+//! the dispatcher dispatches events to such listeners.
+//! While most types need to implement the `Listener`-trait,
+//! closures can also become a listener.
+
+extern crate hey_listen;
+// `hey_listen` uses parking_lot's `Mutex` instead of `std::sync::Mutex`.
+extern crate parking_lot;
+
+use hey_listen::{PriorityEventDispatcher, Listener, SyncDispatcherRequest};
+use std::{hash::{Hash, Hasher}, mem::discriminant, sync::Arc};
+use parking_lot::Mutex;
+
+// This is our event-enum, it will represent possible events a single
+// event-dispatcher can dispatch.
+#[derive(Clone, Debug)]
+enum EventEnum {
+    EventVariant(usize),
+}
+
+// We manually implement the `Hash`-, `Eq`-, and `PartialEq` to
+// bypass the value `EventVariant` has.
+impl Hash for EventEnum {
+    fn hash<H: Hasher>(&self, _state: &mut H) {}
+}
+
+impl PartialEq for EventEnum {
+    fn eq(&self, other: &EventEnum) -> bool {
+        discriminant(self) == discriminant(other)
+    }
+}
+
+impl Eq for EventEnum {}
+
+// The actual listener.
+struct ListenerStruct {}
+
+// This implements the `Listener`-trait, enabling the struct above (`ListenerStruct`)
+// to become a trait object when starting listening.
+impl Listener<EventEnum> for ListenerStruct {
+    fn on_event(&mut self, event: &EventEnum) -> Option<SyncDispatcherRequest> {
+        // Do whatever you want inside here, you can even access the struct's fields.
+        // Be aware, the event is immutable.
+
+        match *event {
+            EventEnum::EventVariant(value) => println!("I'm listening and received event with value: {}.", value),
+        }
+
+        // At the end, we have to return and `std::Option` request back to
+        // the dispatcher.
+        // This could be:
+        // - `SyncDispatcherRequest::StopListening` to automatically
+        // stop listening.
+        // - `SyncDispatcherRequest::StopPropagation` stops the dispatcher from dispatching
+        // to other listeners in this instance of dispatching.
+        // - `SyncDispatcherRequest::StopListeningAndPropagation` combines the first and second.
+        // - `None`, equals to sending no request to the dispatcher, everything will stay as it is.
+        None
+    }
+}
+
+fn main() {
+    // Create your listener.
+    let listener = Arc::new(Mutex::new(ListenerStruct {}));
+
+    // Create your dispatcher and define the generic type what the dispatcher
+    // shall accept as dispatchable type, it's our declared `EventEnum` in this
+    // example.
+    let mut dispatcher: PriorityEventDispatcher<u32, EventEnum> = PriorityEventDispatcher::default();
+
+    // Start listening to a listener and decide their dispatch-priority, here level `1`.
+    // The value we give `EventVariant` is not important for adding a listener,
+    // since we implemented `Hash`-, `Eq`-, and `PartialEq`-trait in a manner that
+    // bypasses fields for comparison but compares variants.
+    dispatcher.add_listener(EventEnum::EventVariant(0), &listener, 1);
+
+    // If you want to work with a closure, you can do the following:
+    let listening_closure = Box::new(move |event: &EventEnum| {
+        // Be aware, since enum's variants are no types,
+        // whenever you want to work with the enum,
+        // you need to pattern-match it of if-let-bind in order to find its variant,
+        // even if you listen to only one variant.
+        let event_name = match *event {
+            EventEnum::EventVariant(value) => value.to_string(),
+        };
+
+        println!("Received event-variant with value: {}!", event_name);
+
+        // As we did in the `Listener`-implementation:
+        None
+    });
+
+    // Closures require the `add_fn`-method instead `add_listener`.
+    dispatcher.add_fn(EventEnum::EventVariant(0), listening_closure, 3);
+
+    // Dispatches our events to all listeners.
+    dispatcher.dispatch_event(&EventEnum::EventVariant(1));
+}
